2fa1f603b41f3ca50d40429b6906c0e52256ef14
wenop <wenoptics@163.com>
2016-07-23 16:20:17 +0800

    bug fixed

--- SingleCard.py
+++ SingleCard.py
@@ -18,14 +18,13 @@ from lxml import etree
 '''
 
 
-def get_html_string(url) :
-
+def get_html_string(url):
     # use iPhone
     user_agent = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
 
-    headers = { 'User-Agent' : user_agent }
+    headers = {'User-Agent': user_agent}
 
-    data = None #urllib.parse.urlencode(values)
+    data = None  # urllib.parse.urlencode(values)
 
     req = urllib.request.Request(url, data, headers)
 
@@ -36,7 +35,6 @@ def get_html_string(url) :
 
 
 def htmlProcessor_wikiHistory(html):
-
     parser = etree.HTMLParser()
     tree = etree.parse(StringIO(html), parser)
 
@@ -45,19 +43,20 @@ def htmlProcessor_wikiHistory(html):
     revisionTime = revisionTime[0].text
     print("updateTime: %s" % revisionTime)
 
-def htmlProcessor_cardInfo(html):
 
+def htmlProcessor_cardInfo(html):
     parser = etree.HTMLParser()
     tree = etree.parse(StringIO(html), parser)
 
     # fetch card name
     cardName = tree.getroot().xpath('//*[@id="WikiaPageHeader"]/div[1]/div[1]/h1')
     cardName = cardName[0].text
+    print("Card: %s" % cardName)
 
     # fetch the image
     cardImageURL = tree.getroot().xpath('//*[@id="mw-content-text"]/div[@class="center"]/div[@class="floatnone"]/img')
     cardImageURL = cardImageURL[0].attrib.get('src')
-    print(cardImageURL)
+    print("Image URL:", cardImageURL)
 
     # data from attrib table
     tblCount = tree.getroot().xpath('//table[@id="unit-attributes-table"]/tr[1]/th')
@@ -65,35 +64,35 @@ def htmlProcessor_cardInfo(html):
     print('count of attribute table:', tblCount)
 
     varAttrTbl = {}
-    for i in range(1, tblCount):
-        tblTitle = tree.getroot().xpath('//table[@id="unit-attributes-table"]/tr[1]/th[%d]' % i )
-        tblContent = tree.getroot().xpath('//table[@id="unit-attributes-table"]/tr[2]/td[%d]' % i )
+    for c in range(tblCount):
+        i = c+1
+        tblTitle = tree.getroot().xpath('//table[@id="unit-attributes-table"]/tr[1]/th[%d]' % i)
+        tblContent = tree.getroot().xpath('//table[@id="unit-attributes-table"]/tr[2]/td[%d]' % i)
         tblContent2 = tree.getroot().xpath('//table[@id="unit-attributes-table"]/tr[2]/td[%d]/a' % i)
-        tblContent3 = tree.getroot().xpath('//table[@id="unit-attributes-table"]/tr[2]/td[%d]/span' % i)
+        tblContent3 = tree.getroot().xpath('//table[@id="unit-attributes-table"]/tr[2]/td[%d]/a/span' % i)
 
         tblTitle = tblTitle[0].text
+        tblContent = tblContent[0].text
         if tblContent2:
-            if tblContent3:
-                tblContent = tblContent3[0].text
-            else:
-                tblContent = tblContent2[0].text
-        else:
-            tblContent = tblContent[0].text
-        #print(" %d , %s, %s" % (i, tblTitle, tblContent))
+            tblContent = tblContent2[0].text
+        if tblContent3:
+            tblContent = tblContent3[0].text
+        # print(" %d , %s, %s" % (i, tblTitle, tblContent))
         varAttrTbl[tblTitle.strip()] = tblContent.strip()
 
-
+    print("Rarity: %s" % varAttrTbl.get('Rarity'))
+    print("Type: %s" % varAttrTbl.get('Type'))
+    print("Elixir Cost: %s" % varAttrTbl.get('Cost'))
 
 
 if __name__ == '__main__':
-
-    url = 'http://clashroyale.wikia.com/wiki/Goblin_Barrel?action=history'
-    html = get_html_string(url)
-    html = html.decode("utf8")
-    htmlProcessor_wikiHistory(html)
-
     url = 'http://clashroyale.wikia.com/wiki/Goblin_Barrel'
     html = get_html_string(url)
     html = html.decode("utf8")
     htmlProcessor_cardInfo(html)
+
+    url += '?action=history'
+    html = get_html_string(url)
+    html = html.decode("utf8")
+    htmlProcessor_wikiHistory(html)
 
